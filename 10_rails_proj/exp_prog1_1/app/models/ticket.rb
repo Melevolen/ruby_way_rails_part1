80115f39f222438f146662c90230232e85663f1d
Ilya Dyakov <i.dyakov@zmeke.com>
2016-10-25 13:37:51 +0300

    correct models

--- 10_rails_proj/exp_prog1_1/app/models/ticket.rb
+++ 10_rails_proj/exp_prog1_1/app/models/ticket.rb
@@ -3,8 +3,8 @@ class Ticket < ApplicationRecord
   belongs_to :user, inverse_of: :tickets
   belongs_to :train, inverse_of: :tickets
   
-  belongs_to :first_railway_station_id, class_name: 'RailwayStation', foreign_key: :first_railway_station_id, inverse_of: :trains, optional: true
-  belongs_to :last_railway_station_id, class_name: 'RailwayStation', foreign_key: :first_railway_station_id, inverse_of: :trains, optional: true
+  belongs_to :first_railway_station, class_name: 'RailwayStation', foreign_key: :first_railway_station_id, inverse_of: :trains, optional: true
+  belongs_to :last_railway_station, class_name: 'RailwayStation', foreign_key: :last_railway_station_id, inverse_of: :trains, optional: true
 
 end
 
--- 10_rails_proj/exp_prog1_1/app/models/user.rb
+++ 10_rails_proj/exp_prog1_1/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+
   has_many :tickets, inverse_of: :user
   has_many :trains, through: :tickets
+
+  validates :name, presence: true
+
 end
